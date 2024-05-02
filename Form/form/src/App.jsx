4a6f517b29b30bc711ae86711a942797bdc357ba
rPatil-html <rohitpatit1137@gmail.com>
2024-05-02 16:16:43 +0530

    rinku

--- Form/form/src/App.jsx
+++ Form/form/src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [name,setname]=useState("");
+  const [email,setemail]=useState("");
+  const [password,setpassword]=useState("");
+const hendlechendname=(e)=>{
+  setname(e.target.value)
+}
+const handleEmail=(e)=>{
+  setemail(e.target.value)
+}
+const handlePassword=(e)=>{
+  setpassword(e.target.value)
+}
+  const handelsubimt=(e)=>{
+    e.preventDefault()
+    const userdata={
+      name:name,
+      email:email,
+      password:password
+    }
+    console.log(userdata)
+    
+
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <>  
+           <h1>Form Data</h1>
+          <form onSubmit={handelsubimt}>
+            <input type="text" placeholder="name" value={name} onChange={hendlechendname} /><br /><br />
+            <input type="text"  placeholder="emali" value={email} onChange={handleEmail}/><br /><br />
+            <input type="text" placeholder="password" value={password} onChange={handlePassword}/><br /><br />
+            <button>subimt</button>
+        </form>
     </>
   )
 }
---
+++ Form/form/src/Coumponet/Form2.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function Form2() {
+
+  const setData=()
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Form2
+
+
